46914c5f68bcd6fc240e70b58f2718a9f3848603
Alexandra Ramirez <alexa090114@gmail.com>
2026-04-23 23:12:37 -0500

    "feat: agregar funcion mostrar con switch y ejercicio1 con ciclo for del 1 al 5"

---
+++ repaso.js
@@ -0,0 +1,15 @@
+function mostrar(opcion){
+    switch (opcion){
+        case 1: ejercicio1();
+            break;
+        
+    }
+
+}
+
+function ejercicio1(){
+    for(let i = 1;  i <= 5; i++){
+        console.log(i)
+
+    }
+}
