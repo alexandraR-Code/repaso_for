811e79b8422b163572539377a5fda47fb9b69211
Alexandra Ramirez <alexa090114@gmail.com>
2026-04-25 14:06:12 -0500

    "Se implementa generación dinámica de tabla de multiplicar con for e innerHTML"

--- Tablas/multiplicar.js
+++ Tablas/multiplicar.js
@@ -3,7 +3,7 @@ function generarTablas(){
     let contenedor = document.getElementById("tablas");
     
     for(let i = 1; i<=10; i++){
-        contenido += `<p>3 x ${i}= ${3*i}</p>`;
+        contenido += `<p>5 x ${i}= ${5*i}</p>`;
     }
     contenedor.innerHTML = contenido;
 }
